8748e592ea5425c8cbb3ab4cd56765bade3101c3
Ader-rick <yang1614220048@gmail.com>
2025-02-24 20:58:31 +0800

    v0.2,创建了一个url-master的客户端和后端。暂未添加节点的交流

---
+++ services/url-master/src/init_config.py
@@ -0,0 +1,18 @@
+import os
+import sqlite3
+
+def init_db(DB_PATH):
+    if not os.path.exists(DB_PATH):
+        con = sqlite3.connect(DB_PATH)
+        print('创建并连接成功')
+        cur = con.cursor()
+        cur.execute("CREATE TABLE URL(ID INT PRIMARY KEY NOT NULL,URL TEXT NO NULL)")
+        cur.execute("INSERT INTO URL (ID, URL) VALUES (1, 'https://www.example.com')")
+        cur.execute("INSERT INTO URL (ID, URL) VALUES (2, 'https://www.google.com')")
+        cur.execute("INSERT INTO URL (ID, URL) VALUES (3, 'https://www.github.com')")
+        cur.execute("INSERT INTO URL (ID, URL) VALUES (4, 'https://www.flask.pocoo.org')")
+        cur.execute("INSERT INTO URL (ID, URL) VALUES (5, 'https://www.python.org')")
+        cur.execute("INSERT INTO URL (ID, URL) VALUES (6, 'https://www.sqlite.org')")
+        con.commit()
+        con.close()
+        print("输入成功")
